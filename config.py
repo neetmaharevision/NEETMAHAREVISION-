bca130111b1fefa0ce9bb68f6dce15b3b57a26df
jbbot2210 <jbbot2210@gmail.com>
2025-04-23 16:39:39 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-api_id = "27058143"
-api_hash = "c569ea4f5ade3fc70d2bb1fd162d9dc9"
-bot_token = "7692348851:AAFauT5HaY-bllHEDe8pFcSfj_mvh8pTj2I"
-auth_users = [6148865829,5817712634]
+api_id = "api id"
+api_hash = "api hadh"
+bot_token = "bot token"
+auth_users = [7890781002]
+sudo_users = [7890781002]
