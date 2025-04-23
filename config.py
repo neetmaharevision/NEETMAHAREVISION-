ecd1ae960205a5ca0399d82f6ddcd088d455d114
jbbot2210 <jbbot2210@gmail.com>
2025-04-23 16:45:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "27058143"
+api_hash = "c569ea4f5ade3fc70d2bb1fd162d9dc9"
+bot_token = "7975806032:AAELRObQu8mefwj9B1boOaEjskqD2IKU_uo"
+auth_users = [8144269730]
+sudo_users = [8144269730]
