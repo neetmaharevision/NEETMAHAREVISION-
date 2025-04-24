459504605cd069ff35b530ae5bc1687f30c11406
neetmaharevision <neetmaharevision@gmail.com>
2025-04-24 17:25:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "27058143"
 api_hash = "c569ea4f5ade3fc70d2bb1fd162d9dc9"
-bot_token = "7975806032:AAELRObQu8mefwj9B1boOaEjskqD2IKU_uo"
+bot_token = "7880279422:AAFTiGPGTXGLzQGrxr0337aHJIDhnVUXGM8"
 auth_users = [8144269730]
 sudo_users = [8144269730]
